37f9e00748fc10b08c2feb8eda917a7a5940d17a
Ucomputer <114789071+Ucomputer@users.noreply.github.com>
2022-12-29 12:42:42 -0600

    Add files via upload

---
+++ crearachivo.0.0.1.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+
+
+menu() {
+
+
+printf "[] \n"
+printf "[] \n"
+printf "[] \n"
+printf "[] \n"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+
+if [[ $option == 1 || $option == 01 ]]; then
+server="pattern"
+start1
+elif [[ $option == 2 || $option == 02 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/3aP8DJD > /dev/null 2>&1 
+elif [[ $option == 3 || $option == 03 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhwhatschat > /dev/null 2>&1 
+elif [[ $option == 4 || $option == 04 ]]; then
+fixer
+elif [[ $option == 5 || $option == 05 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhfollowus > /dev/null 2>&1 
+elif [[ $option == 6 || $option == 06 ]]; then
+cd $HOME/hacklock/core/update/
+bash update.sh
+elif [[ $option == 7 || $option == 07 ]]; then
+cd $HOME/hacklock/core/about/
+bash about.sh
+elif [[ $option == 8 || $option == 08 ]]; then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1 
+elif [[ $option == 9 ]]; then
+exit 1
+else
+printf "\e[1;93m [!] Invalid option!\e[0m\n"
+sleep 1
+clear
+banner
+dependencies
+menu
+fi
+}
+
+archivocp() {
+
+printf "Nombre del archivo : \n"
+printf "[] \n"
+printf "[] \n"
+printf "[1] \n"
+printf "[2] \n"
+printf "[3] \n"
+printf "[4] Nuevo archivo \n"
+printf "[] \n"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+
+if [[ $option == 1 || $option == 01 ]]; then
+cp
+elif [[ $option == 2 || $option == 02 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/3aP8DJD > /dev/null 2>&1 
+elif [[ $option == 3 || $option == 03 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhwhatschat > /dev/null 2>&1 
+elif [[ $option == 4 || $option == 04 ]]; then
+archivocp
+elif [[ $option == 5 || $option == 05 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhfollowus > /dev/null 2>&1 
+elif [[ $option == 6 || $option == 06 ]]; then
+cd $HOME/hacklock/core/update/
+bash update.sh
+elif [[ $option == 7 || $option == 07 ]]; then
+cd $HOME/hacklock/core/about/
+bash about.sh
+elif [[ $option == 8 || $option == 08 ]]; then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1 
+elif [[ $option == 9 || $option == 09 ]]; then
+
+elif [[ $option == 0 || $option == 00 ]]; then
+exit 1
+archivocp
+fi
+}
+
+archivo() {
+
+printf "Nombre del archivo : \n"
+printf "[] \n"
+printf "[] \n"
+printf "[] \n"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+
+if [[ $option == 1 || $option == 01 ]]; then
+server="pattern"
+start1
+elif [[ $option == 2 || $option == 02 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/3aP8DJD > /dev/null 2>&1 
+elif [[ $option == 3 || $option == 03 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhwhatschat > /dev/null 2>&1 
+elif [[ $option == 4 || $option == 04 ]]; then
+fixer
+elif [[ $option == 5 || $option == 05 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhfollowus > /dev/null 2>&1 
+elif [[ $option == 6 || $option == 06 ]]; then
+cd $HOME/hacklock/core/update/
+bash update.sh
+elif [[ $option == 7 || $option == 07 ]]; then
+cd $HOME/hacklock/core/about/
+bash about.sh
+elif [[ $option == 8 || $option == 08 ]]; then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1 
+elif [[ $option == 9 ]]; then
+exit 1
+else
+printf "\e[1;93m [!] Invalid option!\e[0m\n"
+sleep 1
+clear
+banner
+dependencies
+menu
+fi
+}
+
+archivo() {
+
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+
+if [[ $option == 1 || $option == 01 ]]; then
+server="pattern"
+start1
+elif [[ $option == 2 || $option == 02 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/3aP8DJD > /dev/null 2>&1 
+elif [[ $option == 3 || $option == 03 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhwhatschat > /dev/null 2>&1 
+elif [[ $option == 4 || $option == 04 ]]; then
+fixer
+elif [[ $option == 5 || $option == 05 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhfollowus > /dev/null 2>&1 
+elif [[ $option == 6 || $option == 06 ]]; then
+cd $HOME/hacklock/core/update/
+bash update.sh
+elif [[ $option == 7 || $option == 07 ]]; then
+cd $HOME/hacklock/core/about/
+bash about.sh
+elif [[ $option == 8 || $option == 08 ]]; then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1 
+elif [[ $option == 9 ]]; then
+exit 1
+else
+printf "\e[1;93m [!] Invalid option!\e[0m\n"
+sleep 1
+clear
+banner
+dependencies
+menu
+fi
+}
+
+archivo() {
+
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+
+if [[ $option == 1 || $option == 01 ]]; then
+server="pattern"
+start1
+elif [[ $option == 2 || $option == 02 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/3aP8DJD > /dev/null 2>&1 
+elif [[ $option == 3 || $option == 03 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhwhatschat > /dev/null 2>&1 
+elif [[ $option == 4 || $option == 04 ]]; then
+fixer
+elif [[ $option == 5 || $option == 05 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhfollowus > /dev/null 2>&1 
+elif [[ $option == 6 || $option == 06 ]]; then
+cd $HOME/hacklock/core/update/
+bash update.sh
+elif [[ $option == 7 || $option == 07 ]]; then
+cd $HOME/hacklock/core/about/
+bash about.sh
+elif [[ $option == 8 || $option == 08 ]]; then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1 
+elif [[ $option == 9 ]]; then
+exit 1
+else
+printf "\e[1;93m [!] Invalid option!\e[0m\n"
+sleep 1
+clear
+banner
+dependencies
+menu
+fi
+}
+
+cp() {
+
+$a >> $b
+
+printf "[] \n"
+printf "[] \n"
+printf "[] \n"
+printf "[] \n"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+
+if [[ $option == 1 || $option == 01 ]]; then
+cp
+elif [[ $option == 2 || $option == 02 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/3aP8DJD > /dev/null 2>&1 
+elif [[ $option == 3 || $option == 03 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhwhatschat > /dev/null 2>&1 
+elif [[ $option == 4 || $option == 04 ]]; then
+fixer
+elif [[ $option == 5 || $option == 05 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhfollowus > /dev/null 2>&1 
+elif [[ $option == 6 || $option == 06 ]]; then
+cd $HOME/hacklock/core/update/
+bash update.sh
+elif [[ $option == 7 || $option == 07 ]]; then
+cd $HOME/hacklock/core/about/
+bash about.sh
+elif [[ $option == 8 || $option == 08 ]]; then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1 
+elif [[ $option == 9 || $option == 09 ]]; then
+
+elif [[ $option == 0 || $option == 00 ]]; then
+exit 1
+cp
+fi
+}
+
+
+opciones() {
+
+
+printf "[] 
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+
+if [[ $option == 1 || $option == 01 ]]; then
+server="pattern"
+start1
+elif [[ $option == 2 || $option == 02 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/3aP8DJD > /dev/null 2>&1 
+elif [[ $option == 3 || $option == 03 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhwhatschat > /dev/null 2>&1 
+elif [[ $option == 4 || $option == 04 ]]; then
+fixer
+elif [[ $option == 5 || $option == 05 ]]; then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhfollowus > /dev/null 2>&1 
+elif [[ $option == 6 || $option == 06 ]]; then
+cd $HOME/hacklock/core/update/
+bash update.sh
+elif [[ $option == 7 || $option == 07 ]]; then
+cd $HOME/hacklock/core/about/
+bash about.sh
+elif [[ $option == 8 || $option == 08 ]]; then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1 
+elif [[ $option == 9 ]]; then
+exit 1
+else
+printf "\e[1;93m [!] Invalid option!\e[0m\n"
+sleep 1
+clear
+banner
+dependencies
+menu
+fi
+}
+
+
+menu
